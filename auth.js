e14ad733d9d48917c3e42ee541c2e5f88eb03bfb
ankit_sony3@hotmail.com <ankit@Ankits-MacBook-Pro-2.local>
2020-12-18 23:16:07 +0530

    auth service with login mutation

---
+++ auth.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildFederatedSchema } from "@apollo/federation";
+
+import { typeDefs, resolvers } from "./authSchema";
+
+const app = express();
+app.use(express.json());
+
+const apolloServer = new ApolloServer({
+  schema: buildFederatedSchema({ typeDefs, resolvers }),
+  context: ({ req, res }) => ({ req, res }),
+});
+
+apolloServer.applyMiddleware({ app, cors: false });
+
+app.listen(Number(process.env.AUTH_PORT));
+console.log(
+  `Auth server started at domain ${process.env.AUTH_DOMAIN}:${process.env.AUTH_PORT}`
+);
